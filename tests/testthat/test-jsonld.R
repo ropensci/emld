e100af5312c2419453ac3d7408265eb1ee8a34d8
Carl Boettiger <cboettig@gmail.com>
2017-11-05 21:59:53 -0800

    comment out un-implemented test elements

--- tests/testthat/test-jsonld.R
+++ tests/testthat/test-jsonld.R
@@ -2,6 +2,7 @@ library(jsonld)
 library(jsonlite)
 library(emljson)
 library(magrittr)
+library(xml2)
 
 context <- '{"@context": {"@vocab": "http://ecoinformatics.org/"}}'
 ex <- system.file("extdata/hf205.json", package = "emljson")
@@ -21,8 +22,25 @@ xml <- system.file("extdata/hf205.xml", package = "emljson")
 xml %>%
   xml2::read_xml() %>%
   xml2::as_list() %>%
-  list(eml = .) %>%
-  xml2::as_xml_document() %>%
-  xml_set_namespace("eml", "eml://ecoinformatics.org/eml-2.1.1") %>%
-  write_xml("test.xml")
-testthat::expect_true(EML::eml_validate(xml))
+  list('eml' = .) %>%
+  xml2::as_xml_document()
+
+## fails to set name to eml:eml, fails to keep ns on attribute name xsi:schemaLocation
+#%>%
+#  xml2::xml_set_namespace("eml", "eml://ecoinformatics.org/eml-2.1.1") %>%
+#  xml2::write_xml("test.xml", ns="eml")
+#testthat::expect_true(EML::eml_validate("test.xml"))
+
+
+
+#system.file("extdata/hf205.json", package = "emljson") %>%
+#  jsonld_expand() %>%
+#  jsonld_compact(context) %>%
+#  fromJSON() %>%
+#  getElement("eml") %>%
+#  list(eml = .) %>%
+#  xml2::as_xml_document() %>%
+
+#  xml2::xml_set_namespace("eml", "eml://ecoinformatics.org/eml-2.1.1") %>%
+#  xml2::write_xml("test.xml")
+#testthat::expect_true(EML::eml_validate("text.xml"))
