93add18763b8633bd1a0c8990abda2da1226f1d1
Carl Boettiger <cboettig@gmail.com>
2018-01-02 09:14:46 -0800

    fix bug when parsing json via `as_emld()`

--- R/as_emld.R
+++ R/as_emld.R
@@ -12,7 +12,7 @@ as_emld <- function(x){
     if(file.exists(x)){
       if(grepl("\\.xml$", x)){
         x <- xml2::read_xml(x)
-      } else if(grepl("\\.xml$", x)){
+      } else if(grepl("\\.json$", x)){
         x <- jsonlite::read_json(x)
       } else {
         stop(paste("extension for", basename(x), "not recognized"))
@@ -22,7 +22,9 @@ as_emld <- function(x){
 
   ## Convert json or xml_document to the S3 emld object
   if(is(x, "json")){
-    return(jsonlite::fromJSON(x, simplifyVector = FALSE))
+    emld <- jsonlite::fromJSON(x, simplifyVector = FALSE)
+    class(emld) <- c("emld", "list")
+    return(emld)
   }
 
   else if(is(x, "xml_document")){
@@ -33,11 +35,11 @@ as_emld <- function(x){
     emld <- as_jsonlist(x)
 
     ## Set a base type  Do we want to declare `@type`?
-    emld <- c("@type" = "eml", emld)
+    emld <- c("@type" = "EML", emld)
 
     ## Set up the JSON-LD context
     if(is.null(emld[["#xmlns"]])){
-      emld[["#xmlns"]] <- "http://ecoinformatics.org/"
+      emld[["#xmlns"]] <- "eml://ecoinformatics.org/eml-2.1.1/"
     }
     emld <- add_context(emld)
     class(emld) <- c("emld", "list")
