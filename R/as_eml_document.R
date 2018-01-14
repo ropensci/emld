9c22193dee32931d08a0e045ea2e4623613191c5
Carl Boettiger <cboettig@gmail.com>
2018-01-13 21:40:24 -0800

    first pass at TextType as literal

--- R/as_eml_document.R
+++ R/as_eml_document.R
@@ -1,21 +1,6 @@
-
-as_eml_document <- function(x, ...) {UseMethod("as_eml_document")}
-
-#' @importFrom jsonlite fromJSON
-as_eml_document.character <- function(x, ...){
-  as_eml_document(jsonlite::fromJSON(x, simplifyVector = FALSE))
-}
-as_eml_document.json <- function(x, ...){
-  as_eml_document(jsonlite::fromJSON(x, simplifyVector = FALSE))
-}
-
-as_eml_document.emld <- function(x, ...) {
-  as_eml_document.list(x, ...)
-}
-
 #' @importFrom xml2 xml_add_child xml_set_attr xml_new_document
 #' @importFrom xml2 xml_set_namespace xml_root xml_find_first
-as_eml_document.list <- function(x, ...) {
+as_eml_document <- function(x, ...) {
 
   x[["@type"]] <- NULL
   if(is.null(x[["#schemaLocation"]]))
@@ -29,21 +14,6 @@ as_eml_document.list <- function(x, ...) {
   doc
 }
 
-## Identical to as_xml_document methods
-#' @importFrom xml2 xml_new_root
-as_eml_document.xml_node <- function(x, ...) {
-  xml2::xml_new_root(.value = x, ..., .copy = TRUE)
-}
-as_eml_document.xml_nodeset <- function(x, root, ...) {
-  doc <- xml2::xml_new_root(.value = root, ..., .copy = TRUE)
-  for (i in seq_along(x)) {
-    xml2::xml_add_child(doc, x[[i]], .copy = TRUE)
-  }
-  doc
-}
-as_eml_document.xml_document <- function(x, ...) {
-  x
-}
 
 
 drop_nulls <- function(x){
@@ -59,12 +29,30 @@ add_node <- function(x, parent, tag) {
       return() ## bc we call add_node after already eval on is.atomic
     }
 
-    #x <- drop_nulls(x)
-    x <- sort_properties(x, tag)
+    #x <- drop_nulls(x)  ## FIXME: drop any numbered tags
 
+    ## unwrap group_by_key sets
     if(!is.null(names(x)) & length(x) > 0){
       parent <- xml2::xml_add_child(parent, tag)
     }
+
+    ## Handle text-type explicitly, parsing back into XML
+    ## Note: length-1 case gets handled as an atomic character string
+    if(tag %in% c("para", "section")){
+      if(length(x) > 1) {
+        if(grepl("^<\\w+>", x[[1]])){ ## starts with a tag, assume XML
+          for(i in seq_along(x)){
+            n <- read_xml(paste0("<", tag, ">", x[[i]], "</", tag, ">"))
+            xml2::xml_add_child(parent, n, .copy = TRUE)
+          }
+          return()
+        }
+      }
+    }
+
+    x <- sort_properties(x, tag)
+
+    ## Handle all other values (nodes and attributes)
     update_tag <- !is.null(names(x))
     for(i in seq_along(x)){
       if(is.atomic(x[[i]])){
@@ -75,6 +63,10 @@ add_node <- function(x, parent, tag) {
       }
       add_node(x[[i]], parent, tag)
     }
+
+
+
+
 }
 
 serialize_atomics <- function(x, parent, tag, key){
@@ -82,6 +74,18 @@ serialize_atomics <- function(x, parent, tag, key){
     textType <- xml2::xml_add_child(parent, tag)
     return(xml2::xml_set_text(textType, x))
   }
+
+  ## SPECIAL, handle length-1 text types
+  if(key %in% c("para", "section")){
+      if(grepl("^<\\w+>", x)){ ## starts with a xml element, assume XML
+        n <- read_xml(paste0("<", key, ">", x, "</", key, ">"))
+        xml2::xml_add_child(parent, n, .copy = TRUE)
+        return()
+      }
+  }
+
+
+
   if(grepl("^@*id$", key)){
     ## Skip `@id` element if uses a json-ld local id
     if(grepl("^_:b\\d+", x)){
@@ -101,7 +105,8 @@ serialize_atomics <- function(x, parent, tag, key){
     ## Text-type atomics ##
     if(!is_attr){
       if(grepl("^#\\w+", tag)){
-        ## special case where JSON-LD repeats node name (for grouped nodes with attributes, e.g. url)
+        ## special case where JSON-LD repeats node name
+        ## (for grouped nodes with attributes, e.g. url)
         xml2::xml_set_text(parent, x)
       } else {
         textType <- xml2::xml_add_child(parent, key)
@@ -136,8 +141,8 @@ sort_properties <- function(x, tag){
   if(!all(n %in% order))
     return(x)
 
-  fixed <- names(sort(vapply(n,
-                             function(i) which(i == order), integer(1))))
+  fixed <- names(sort(
+    vapply(n,function(i) which(i == order), integer(1))))
 
   c(x[attrs],nodes[fixed])
 
---
+++ tests/testthat/test-textType.R
@@ -0,0 +1,43 @@
+testthat::context("textType")
+
+
+
+me <- list(individualName = list(givenName = "Carl", surName = "Boettiger"))
+eml <- list(dataset = list(
+  title = "dataset title",
+  contact = me,
+  creator = me),
+  "#system" = "doi",
+  "#packageId" = "10.xxx")
+
+testthat::test_that("We have created a minimal, valid EML file", {
+  as_xml(eml, "ex.xml")
+  testthat::expect_true( EML::eml_validate("ex.xml") )
+  unlink("ex.xml")
+})
+
+
+testthat::test_that("We can add abstract as text string", {
+  eml$dataset$abstract <- "This is a short abstract for this dataset."
+  as_xml(eml, "ex.xml")
+  testthat::expect_true( EML::eml_validate("ex.xml") )
+  unlink("ex.xml")
+})
+
+testthat::test_that("We can add abstract with multiple paragraphs", {
+  eml$dataset$abstract <-
+    list(para = list(
+      "This is a short abstract for this dataset.",
+      "This is the second paragraph"))
+  as_xml(eml, "ex.xml")
+  testthat::expect_true( EML::eml_validate("ex.xml") )
+  unlink("ex.xml")
+})
+
+
+f <- system.file("tests/eml-text.xml", package="emld")
+text <- as_emld(f)
+as_json(text, "text.json")
+as_xml(text, "text.xml")
+unlink("text.xml")
+unlink("text.json")
