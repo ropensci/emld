6b9e126b95300d6ad762cad22985c1aae22d1fa1
Carl Boettiger <cboettig@gmail.com>
2018-01-25 00:22:47 +0000

    remove antiquated test

--- tests/testthat/test-base.R
+++ tests/testthat/test-base.R
@@ -29,33 +29,3 @@ test_that("we can roundtrip JSON-LD expansion and compaction", {
 })
 
 
-test_that("We can validate after roundtrip with default xml2 methods", {
-
-  ## In order to roundtrip, we need to fix some mangling of read_xml and as_list
-  ## The schemaLocation attribute needs to get its namespace prefix back.
-  ## and the root eml node needs to be first be recreated, and then
-  ## to get its namespace prefix back.  ICK
-  rename_attr <- function(x, old, new){
-    attr(x, new) <- attr(x, old)
-    attr(x, old) <- NULL
-    x
-  }
-  rename_root <- function(xml, n){
-    root <- xml_root(xml)
-    xml_name(root) <- n
-    xml
-  }
-
-  xml <- system.file("extdata/example.xml", package = "emld")
-  xml %>%
-    xml2::read_xml() %>%
-    xml2::as_list() %>%
-    rename_attr("schemaLocation", "xsi:schemaLocation") %>%
-    list('eml' = .) %>%
-    xml2::as_xml_document() %>%
-    rename_root("eml:eml") %>%
-    xml2::write_xml("test.xml")
-
-  testthat::expect_true(eml_validate("test.xml"))
-  unlink("test.xml")
-})
