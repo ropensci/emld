7ca83039aa0a83ee14dd40ee351aa55cf5296b8f
Carl Boettiger <cboettig@gmail.com>
2018-04-26 23:28:17 -0700

    first pass

--- data-raw/create-eml-2.2.0.json.R
+++ data-raw/create-eml-2.2.0.json.R
@@ -1,55 +1,50 @@
 library(tidyverse)
+library(glue)
 library(xml2)
 library(jsonlite)
 xsd_files <- list.files("inst/xsd/eml-2.2.0", full.names = TRUE)
 
-xsd <- xsd_files[[4]]
 
-
-map(xsd_files[4], function(xsd){
-
+map(xsd_files, function(xsd){
 
   xml <- read_xml(xsd)
   ## only when complexType appears with names is it being defined, otherwise it's nested
   complex_types <- xml_find_all(xml, "//xs:complexType[@name]")
   who <- map_chr(complex_types, xml_attr, "name")
-  ## Get annotation
 
-  ## Get attributes of the complexType
+  ## FIXME: unwrap xs:group.
 
-  ## Get elements of the complexType:
-  ## all xs:elements which are descendants of said complexType
-
+  ## Consider also parsing: use, default, type for function def.
   def <- map(complex_types, function(node){
+    #docstring <- xml_children(xml_child(xml_child(node, "xs:annotation"), "xs:appinfo"))
 
-    docstring <- xml_child(node, "xs:annotation")
 
+    ## Attributes of the complexType
     children <- xml_children(node)
     is_attr <- map_chr(children, xml_name) == "attribute"
-    children[is_attr]
+    attribs <- as.character(na.omit(map_chr(children[is_attr], xml_attr, "name")))
+    attribs <- map_chr(attribs, ~ paste0("#", .x)) # has len 0 when attribs has len 0
 
-    ## parse attribute name, use, default, type for function def.
 
-    name <- xml_name(node)
-
-    ## descend to first xs:element, return it and it's siblings
+    ## Get elements of the complexType:
+    ## all xs:elements which are descendants of said complexType
     ## Note! xpath always works on full doc, even if given a node.
+    name <- xml_attr(node, "name")
     first <- xml_find_first(xml,
                             glue::glue("//xs:complexType[@name='{name}']//xs:element"))
-    out <- c(list(first), xml_siblings(first))
-    class(out) <- "xml_nodeset"
-    out
+    element_nodes <- c(list(first), xml_siblings(first))
+    class(element_nodes) <- "xml_nodeset"
+
+
+    elements <- map_chr(element_nodes, xml_attr, "name")
+    #elements <- purrr::compact(elements)
+    elements <- as.character(na.omit(elements))
+
+    c(attribs,elements)
   })
+
+  setNames(def, who)
 
   ## xs:group[@ref] means children are defined by children of xs:group[@name]
 
-  out <- map(complex_types, function(node){
-      ## find_all isn't the right strategy here, fails to get only nested subtypes
-      map_chr(xml_find_all(node, "//xs:element"), xml_attr, "name")
-  })
-  names(out) <- who
-  out
-
-
-
-}) %>% toJSON(pretty=TRUE)
+}) %>% unlist(FALSE) %>% write_json("eml-2.2.0.json", pretty=TRUE)
