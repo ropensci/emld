24570db5ebfa38aa35a687bdfa30a8f6d2fd9728
Carl Boettiger <cboettig@gmail.com>
2018-04-29 22:44:33 -0700

    cleaner approach

--- data-raw/create-eml-2.2.0.json.R
+++ data-raw/create-eml-2.2.0.json.R
@@ -4,8 +4,106 @@ library(xml2)
 library(jsonlite)
 xsd_files <- list.files("inst/xsd/eml-2.2.0", full.names = TRUE)
 
+
 # test case
 #xsd <- xsd_files[[4]]
+
+## Basic helper functions #######
+drop_prefix <- function(x) gsub("^\\w+:", "", x)
+
+xml_select <- function(nodeset, pattern = ".*", fn = xml_name,  ...){
+  names <- map_chr(nodeset, fn, ...)
+  match <- grepl(pattern, names)
+  which(match)
+}
+
+# replace the i'th element of list A with list B in flat list
+list_insert <- function(A, B, i){
+  if(length(i)>1)
+    stop("i > 1 case not handled")
+  n <- length(A)
+  if(n < 2)
+    return(B)
+  if(i < n)
+    return(c( A[seq(1,i) - 1], B, A[seq(i+1, n)]))
+  if(i == n)
+    return(c( A[seq(1,i) - 1], B))
+}
+
+xml_drop <- function(nodeset, pattern){
+  i <- xml_select(nodeset, pattern)
+  if(length(i) == 0)
+    return(nodeset)
+  else
+    return(nodeset[-i])
+}
+
+xsd_recursion <- function(node, grouplist = NULL){
+
+  nodeset <- xml_children(node)
+  ## keep attributes
+  ## keep elements
+
+  ## drop docs
+  nodeset <- xml_drop(nodeset, "annotation")
+
+  ## Recursively expand choice and sequence into elements=
+  choice <- xml_select(nodeset, "choice")
+  if(length(choice) > 0){
+    tmp <- xsd_recursion(nodeset[[choice]], grouplist)
+    nodeset <- list_insert(nodeset, tmp, choice)
+  }
+  sequence <- xml_select(nodeset, "sequence")
+  if(length(sequence) > 0){
+    tmp <- xsd_recursion(nodeset[[sequence]], grouplist)
+    nodeset <- list_insert(nodeset, tmp, sequence)
+  }
+
+  ## Replace group reference with group definition
+  group <- xml_select(nodeset, "group")
+  if(length(group) > 0){
+    ref <- drop_prefix(xml_attr(nodeset[[group]], "ref"))
+    nodeset <- list_insert(nodeset, grouplist[[ref]], group)
+  }
+
+  if(!is.null(nodeset))
+    class(nodeset) <- "xml_nodeset"
+  nodeset
+}
+
+group_nodes <- map(xsd_files, function(xsd){
+   xml <- read_xml(xsd)
+   groups <- xml_find_all(xml, "//xs:group[@name]")
+   def <- map(groups, xsd_recursion)
+   who <- map_chr(groups, xml_attr, "name")
+  setNames(def, who)
+
+}) %>% unlist(FALSE)  %>% purrr::compact()
+
+type_nodes <- map(xsd_files, function(xsd){
+   xml <- read_xml(xsd)
+   groups <- xml_find_all(xml, "//xs:complexType[@name]")
+   def <- map(groups, xsd_recursion, group_nodes)
+   who <- map_chr(groups, xml_attr, "name")
+  setNames(def, who)
+
+}) %>% unlist(FALSE)  %>% purrr::compact()
+
+
+
+element_nodes <-
+map(xsd_files, function(xsd){
+  xml <- read_xml(xsd)
+  named_elements <- xml_find_all(xml, "//xs:element[@name]")
+  get_nodes(named_elements, xml, "element", type_nodes, group_nodes)
+}) %>% unlist(FALSE)  %>% purrr::compact()
+
+
+
+
+
+
+
 
 
 type_magic <- function(node){
@@ -51,228 +149,23 @@ type_magic <- function(node){
   type
 }
 
-## Basic helper functions #######
-drop_prefix <- function(x) gsub("^\\w+:", "", x)
-
-xml_select <- function(nodeset, pattern = ".*", fn = xml_name,  ...){
-  names <- map_chr(nodeset, fn, ...)
-  match <- grepl(pattern, names)
-  which(match)
-}
-
-# replace the i'th element of list A with list B in flat list
-list_insert <- function(A, B, i){
-  if(length(i)>1)
-    stop("i > 1 case not handled")
-  n <- length(A)
-  if(n < 2)
-    return(B)
-  if(i < n)
-    return(c( A[seq(1,i) - 1], B, A[seq(i+1, n)]))
-  if(i == n)
-    return(c( A[seq(1,i) - 1], B))
-}
-
-
-xsd_recursion <- function(nodeset, grouplist = NULL){
-
-  ## keep attributes
-  ## keep elements
-
-  ## drop docs
-
-
-  ## Recursively expand choice and sequence into elements=
-  choice <- xml_select(nodeset, "choice")
-  if(length(choice) > 0){
-    tmp <- xsd_recursion(xml_children(nodeset[[choice]]))
-    nodeset <- list_insert(nodeset, tmp, choice)
-  }
-  sequence <- xml_select(nodeset, "sequence")
-  if(length(sequence) > 0){
-    tmp <- xsd_recursion(xml_children(nodeset[[sequence]]))
-    nodeset <- list_insert(nodeset, tmp, sequence)
-  }
-
-  ## Replace group reference with group definition
-  group <- xml_select(nodeset, "group")
-  if(length(group) > 0){
-    ref <- xml_attr(nodeset[[group]], "ref")
-    nodeset <- list_insert(nodeset, grouplist[[ref]], group)
-  }
-
-  if(!is.null(nodeset))
-    class(nodeset) <- "xml_nodeset"
-  nodeset
-}
+## FIXME Some things are missed:  (These create NULLS in the data)
+map(element_nodes, xml_name) %>% unlist() %>% table()
+## lots of unnamed elements too, (on data-valued nodes)
+map(element_nodes, xml_attr, "name") %>% map_int(~ sum(is.na(.x)))
 
 
 
+out <-
+map(element_nodes, function(nodeset){
+  names <- map_chr(nodeset, xml_attr, "name")
+  type <- map_lgl(nodeset, function(n) xml_name(n) == "attribute")
+  names[type] <- map_chr(names[type], ~ paste0("#", .x))
+  as.character(na.omit(names))
+})
 
-
-get_nodes <- function(nodeset, xml, nodename = "element", typelist = NULL, grouplist = NULL){
-  who <- map_chr(nodeset, xml_attr, "name")
-  def <- map(nodeset, function(node){
-    #docstring <- xml_children(xml_child(xml_child(node, "xs:annotation"), "xs:appinfo"))
-
-    name <- xml_attr(node, "name")
-    type <- type_magic(node)
-
-    ## Attributes of the node
-    first <- xml_find_first(xml,
-                            glue::glue("//xs:{nodename}[@name='{name}']//xs:attribute"))
-    if(is(first, "xml_missing")){
-      attribute_nodes <- list()
-    } else {
-      attribute_nodes <- c(list(first), xml_siblings(first))
-    }
-    class(attribute_nodes) <- "xml_nodeset"
-
-    ## Get all the xs:elements OR xs:group which are descendants. order matters!
-    ## Note! xpath always works on full doc, even if given a node.
-    first <- xml_find_first(xml,
-      glue::glue(
-  "//xs:{nodename}[@name='{name}']//xs:element|//xs:{nodename}[@name='{name}']//xs:group"))
-    if(is(first, "xml_missing")){
-      element_nodes <- list()
-       class(element_nodes) <- "xml_nodeset"
-    } else {
-
-      nodeset<- c(list(first), xml_siblings(first))
-      class(nodeset) <- "xml_nodeset"
-      element_nodes <- xsd_recursion(nodeset)
-    }
-
-    if(length(type)>0){
-      type <- typelist[[type]]
-    }
-
-    out <- c(attribute_nodes, element_nodes, type)
-    if(!is.null(out))
-      class(out) <- "xml_nodeset"
-    out
-  })
-  setNames(def, who)
-}
-
-
-
-
-group_nodes <- map(xsd_files, function(xsd){
-  xml <- read_xml(xsd)
-  groups <- xml_find_all(xml, "//xs:group[@name]")
-  get_nodes(groups, xml, "group")
-}) %>% unlist(FALSE)  %>% purrr::compact()
-
-
-type_nodes <- map(xsd_files, function(xsd){
-  xml <- read_xml(xsd)
-  complex_types <- xml_find_all(xml, "//xs:complexType[@name]")
-  get_nodes(complex_types, xml, "complexType", grouplist = group_nodes)
-})  %>% unlist(FALSE) %>% purrr::compact()
-
-
-
-element_nodes <-
-map(xsd_files, function(xsd){
-  xml <- read_xml(xsd)
-  named_elements <- xml_find_all(xml, "//xs:element[@name]")
-  get_nodes(named_elements, xml, "element", type_nodes, group_nodes)
-}) %>% unlist(FALSE)  %>% purrr::compact()
-
-
-
-
-
-
-
-
-
-###############################################################################
-###############################################################################
-###############################################################################
-
-
-
-
-
-
-
-
-## For each member of a nodeset which contains all xs:elements in an XML XSD file
-## Extract all of it's xs:attribute and xs:element members
-get_slots <- function(nodeset, xml, nodename = "element", typelist = NULL, grouplist = NULL){
-  who <- map_chr(nodeset, xml_attr, "name")
-
-  def <- map(nodeset, function(node){
-    #docstring <- xml_children(xml_child(xml_child(node, "xs:annotation"), "xs:appinfo"))
-
-    name <- xml_attr(node, "name")
-    type <- type_magic(node)
-
-    ## Attributes of the node
-    first <- xml_find_first(xml,
-                            glue::glue("//xs:{nodename}[@name='{name}']//xs:attribute"))
-    attribute_nodes <- c(list(first), xml_siblings(first))
-    class(attribute_nodes) <- "xml_nodeset"
-    attribs <- as.character(na.omit(map_chr(attribute_nodes, xml_attr, "name")))
-    attribs <- map_chr(attribs, ~ paste0("#", .x)) # has len 0 when attribs has len 0
-
-    ## Get all the xs:elements OR xs:group which are descendants. order matters!
-    ## Note! xpath always works on full doc, even if given a node.
-    first <- xml_find_first(xml,
-      glue::glue(
-      "//xs:{nodename}[@name='{name}']//xs:element|//xs:{nodename}[@name='{name}']//xs:group"))
-    nodeset<- c(list(first), xml_siblings(first))
-    class(nodeset) <- "xml_nodeset"
-    element_nodes <- xsd_recursion(nodeset)
-
-    elements <-
-      unlist(map(element_nodes, function(n){
-      out <- xml_attr(n, "name")
-      if(is.na(out)){ ## only xs:group don't have names(?)
-        out <- drop_prefix(xml_attr(n, "ref"))
-        if(!is.na(out)){
-          print(out)
-          out <- grouplist[[out]]
-        }
-      }
-      out
-      }))
-    elements <- as.character(na.omit(elements))
-
-    if(length(type)>0){
-      type <- typelist[[type]]
-    }
-
-    c(attribs, elements, type)
-  })
-  setNames(def, who)
-}
-
-
-
-typelist <- map(xsd_files, function(xsd){
-  xml <- read_xml(xsd)
-  complex_types <- xml_find_all(xml, "//xs:complexType[@name]")
-  get_slots(complex_types, xml, "complexType")
-})  %>% unlist(FALSE)
-
-
-
-grouplist <- map(xsd_files, function(xsd){
-  xml <- read_xml(xsd)
-  groups <- xml_find_all(xml, "//xs:group[@name]")
-  get_slots(groups, xml, "group", typelist)
-})  %>% unlist(FALSE)
-
-
-
-map(xsd_files, function(xsd){
-  xml <- read_xml(xsd)
-  named_elements <- xml_find_all(xml, "//xs:element[@name]")
-  get_slots(named_elements, xml, "element", typelist, NULL)
-})  %>%
-  unlist(FALSE) %>%
+out %>%
   ## FIXME Drop duplicate keys
   write_json("data-raw/eml-2.2.0.json", pretty=TRUE)
+
+
