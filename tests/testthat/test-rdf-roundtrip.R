ff314f78e3bb537acd22c1e7bddf4ae102adfb56
Carl Boettiger <cboettig@gmail.com>
2019-03-04 10:42:03 -0800

    use tempfiles everywhere

--- tests/testthat/test-rdf-roundtrip.R
+++ tests/testthat/test-rdf-roundtrip.R
@@ -12,11 +12,14 @@ f <- system.file(file.path("tests",
 
 test_that("We can roundtrip into rdf and validate", {
 
+  eml.json <- tempfile("eml", fileext = ".json")
+  eml.xml <- tempfile("eml", fileext = ".xml")
+
   f %>%
   as_emld() %>%
   as_json() %>%
   rdf_parse("jsonld") %>%
-  rdf_serialize("eml.json", "jsonld")
+  rdf_serialize(eml.json, "jsonld")
 
 ## frame & compact explicitly, even though as_emld should now do this on json input
 frame <- system.file(paste0("frame/",
@@ -25,36 +28,35 @@ frame <- system.file(paste0("frame/",
 context <- system.file(paste0("context/",
                        eml_version(),
                        "/eml-context.json"), package = "emld")
-jsonld_frame("eml.json", frame) %>%
+jsonld_frame(eml.json, frame) %>%
   jsonld_compact(context) %>%
   as_emld() %>%
-  as_xml("eml.xml")
-  expect_true(eml_validate("eml.xml"))
+  as_xml(eml.xml)
+  expect_true(eml_validate(eml.xml))
 })
 
 test_that("We can roundtrip out to rdf-xml", {
+  eml.rdf <- tempfile("eml", fileext = ".rdf")
+  eml.json <- tempfile("eml", fileext = ".json")
+  eml.xml <- tempfile("eml", fileext = ".xml")
 
 ## Into RDF-XML
 f %>%
   as_emld() %>%
   as_json() %>%
   rdf_parse("jsonld") %>%
-  rdf_serialize("eml.rdf", "rdfxml")
+  rdf_serialize(eml.rdf, "rdfxml")
 
 ## Back into jsonld (via rdflib)
-rdf_parse("eml.rdf", "rdfxml") %>%
-  rdf_serialize("eml.json", "jsonld")
+rdf_parse(eml.rdf, "rdfxml") %>%
+  rdf_serialize(eml.json, "jsonld")
 
 
 ## Prove that as_emld frames and compacts automatically:
-as_emld("eml.json") %>%
-  as_xml("eml.xml")
-expect_true(eml_validate("eml.xml"))
+as_emld(eml.json) %>%
+  as_xml(eml.xml)
+expect_true(eml_validate(eml.xml))
 
 })
 
-
-unlink("eml.rdf")
-unlink("eml.json")
-unlink("eml.xml")
 
--- tests/testthat/test-template.R
+++ tests/testthat/test-template.R
@@ -21,16 +21,3 @@ test_that("template knows about internal classes too", {
 })
 
 
-
-
-## test serializing to XML fragment doc
-#f <- "tests/testthat/creator.yml"
-#creator <- yaml::read_yaml(f)
-#doc <- xml2::xml_new_document()
-#add_node(creator, doc, "creator")
-
-## Write element into complete doc
-#eml <- parse_eml(system.file("inst/extdata/example.xml", package="emld"))
-#eml$eml$dataset$creator <- creator
-#doc <- as_eml_document.list(eml)
-
