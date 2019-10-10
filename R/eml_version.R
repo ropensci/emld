a15b47778cd9b4478826e613eda33ee8f5811e3d
Bryce Mecum <petridish@gmail.com>
2019-10-10 13:56:32 -0800

    Update package to reflect new 2.2.0 namespace (#40)
    * Update packge to work with new EML 2.2.0 namespace
    
    Per #39, EML's final schema URI space now begins with https://eml.ecoinformatics.org/{module}-{version} instead of eml:// like in the past.
    
    Both EML and STMML namespaces were updated for EML 2.2.0 docs (STMML gets updated because EML 2 uses STML 1.2)
    
    * Update test.sh to work (broke due to jqr dep)
    
    This adds a custom Dockerfile based on rocker/verse that lets jqr be installed in the container so the tests pass. Couple of other minor tweaks to clean related things up.
    
    * Rebuild pkgdown site
    
    * Re-sync eml-2.2.0.json with final 2.2.0 schema
    
    * Finish updating package for 2.2.0
    
    * Update a portion of the example EML docs to 2.2.0
    
    * Re-build pkgdown site again
    
    * Re-sync stmml xsd (minor whitespace diff)

--- R/eml_version.R
+++ R/eml_version.R
@@ -2,8 +2,8 @@
 
 #' Set or check the EML version default
 #'
-#' @param version EML version, currently either eml-2.2.0 (release candidate),
-#' or eml-2.1.1 (current version, 2011 - 2019)
+#' @param version EML version, currently either eml-2.2.0 (current version), or
+#' eml-2.1.1
 #' @return returns the EML version string. As a side-effect, sets the
 #' requested version as the default version by setting the `emld_db`
 #' variable in [options()].
@@ -11,7 +11,38 @@
 #' eml_version()
 #'
 #' @export
-eml_version <- function(version = getOption("emld_db", "eml-2.1.1")){
+eml_version <- function(version = getOption("emld_db", "eml-2.2.0")){
  options(emld_db = version)
  version
+}
+
+#' Get the XML namespace for a version of EML
+#'
+#' Utility function for use when filling in `xmlns`, `schemaLocation`, or
+#' `vocab` in various representations of EML. This is a little more future-proof
+#' than keeping a dictionary for each version since this won't break on the next
+#'release.
+#'
+#' @param version EML version, currently either eml-2.2.0 (current version) or
+#'eml-2.1.1. Defaults to current version.
+#'
+#' @return returns the full XML namespace URI for the specified version of the
+#' schema
+eml_ns <- function(version = eml_version()) {
+  prefix <- "https://eml.ecoinformatics.org/"
+
+  parts <- tryCatch({
+    as.numeric(strsplit(strsplit(version, "-")[[1]][2], "\\.")[[1]])
+  },
+  error = function(e) {
+    warning("Failed to parse output of eml_version().",
+      " Defaulting to https://eml.ecoinformatics.org.")
+  })
+
+  # Use eml:// form for schema versions equal to or prior to 2.2
+  if (parts[1] <= 2 && parts[2] < 2) {
+    prefix <- "eml://eml.ecoinformatics.org/"
+  }
+
+  paste0(prefix, version)
 }
--- test.sh
+++ test.sh
@@ -7,8 +7,9 @@ testthat::expect_length(status[["errors"]],0)
 testthat::expect_length(status[["warnings"]],0)
 status[["notes"]]' > test.R
 
+docker build . -t emld-tests
 docker run -ti \
--v $(pwd):/home/rstudio/repo \
--w /home/rstudio/repo \
-rocker/verse \
-R -f test.R
+  -v $(pwd):/home/rstudio/repo \
+  -w /home/rstudio/repo \
+  emld-tests \
+  R -f test.R
