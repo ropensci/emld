a1572aebc23d15a915b8dd61d2cad0bc012fb821
Matt Jones <gitcode@magisa.org>
2020-01-24 16:09:32 -0900

    Fix serialization of annotation/references attributes.
    The attributes were incorrectly being added as element children, and now are back to attributes.

--- data-raw/eml_db.R
+++ data-raw/eml_db.R
@@ -9,4 +9,4 @@
 
 eml_db <- list("eml-2.1.1" = jsonlite::read_json("data-raw/eml-2.1.1.json"),
                "eml-2.2.0" = jsonlite::read_json("data-raw/eml-2.2.0.json"))
-devtools::use_data(eml_db, overwrite = TRUE, internal = TRUE)
+usethis::use_data(eml_db, overwrite = TRUE, internal = TRUE)
