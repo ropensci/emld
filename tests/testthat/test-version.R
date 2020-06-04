0a187d88641c8f47cd73e1c100c925bd2f47e23d
Bryce Mecum <petridish@gmail.com>
2020-06-03 19:57:19 -0800

    Tweak eml_version to validate input and prompt when interactive

--- tests/testthat/test-version.R
+++ tests/testthat/test-version.R
@@ -6,3 +6,13 @@ test_that("we can get the namespace for different EML versions", {
   expect_equal(eml_ns("eml-2.2.0"), "https://eml.ecoinformatics.org/eml-2.2.0")
   expect_equal(eml_ns("eml-2.2.1"), "https://eml.ecoinformatics.org/eml-2.2.1")
 })
+
+test_that("we can set the EML version", {
+  expect_equal(eml_version("eml-2.1.1"), "eml-2.1.1")
+  expect_equal(eml_version("eml-2.2.0"), "eml-2.2.0")
+})
+
+test_that("we throw a warning when a user specifies an invalid version", {
+  expect_warning(eml_version("a"))
+  expect_warning(eml_version("2.1.1"))
+})
