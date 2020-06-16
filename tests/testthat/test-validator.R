227af776693dd9ef9fd7f812d6f74f5004bee409
Bryce Mecum <petridish@gmail.com>
2020-06-15 23:08:11 -0800

    Make validate_units use the right eml-unitDictionary
    Ref #56
    
    While `eml_validate` determines the root schema to use when validating, `validate_units` was still defaulting to the version set in `emld_db`. This changes `validate_units` so it uses the appropriate `eml-unitDictionary.xml` file for the root element.
    
    I've also added two test files and a test to exercise this so we know validate_units is working correctly now.

--- tests/testthat/test-validator.R
+++ tests/testthat/test-validator.R
@@ -11,3 +11,12 @@ testthat::test_that("we can use emld validator", {
 
 
 })
+
+
+testthat::test_that("validation uses the right unitDictionary", {
+  f <- system.file("tests", "eml-2.1.1", "invalid", "eml-2.1.1-invalidunit.xml", package = "emld")
+  testthat::expect_warning(eml_validate(f), "not recognized")
+
+  f <- system.file("tests", "eml-2.2.0", "eml-2.2.0-milligramPerLiter.xml", package = "emld")
+  testthat::expect_true(eml_validate(f))
+})
